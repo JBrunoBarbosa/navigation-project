349284b9539379c543383ed808547c7f2e7a22ba
Bruno Barbosa <bruno.barbosa@usemobile.xyz>
2021-05-27 13:11:05 -0300

    feat: implementation main fragment navigation

--- app/src/main/java/bruno_brabosa/convidados/navigation_project/MainFragment.kt
+++ app/src/main/java/bruno_brabosa/convidados/navigation_project/MainFragment.kt
@@ -5,12 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import bruno_brabosa.convidados.navigation_project.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        setUpViewBinding()
+        setUpClicksListeners()
+
+        return binding.root
     }
 
+    private fun setUpViewBinding() {
+        binding = FragmentMainBinding.inflate(layoutInflater)
+    }
+
+    private fun setUpClicksListeners() {
+        binding.apply {
+
+            btnAbove.setOnClickListener {
+                view?.let { view -> Navigation.findNavController(view).navigate(R.id.navigate_to_top_fragment) }
+            }
+
+            btnMiddle.setOnClickListener {
+                view?.let { view -> Navigation.findNavController(view).navigate(R.id.navigate_to_middle_one) }
+            }
+
+            btnBottom.setOnClickListener {
+                view?.let { view -> Navigation.findNavController(view).navigate(R.id.navigate_to_bottom_one) }
+            }
+        }
+    }
 }
