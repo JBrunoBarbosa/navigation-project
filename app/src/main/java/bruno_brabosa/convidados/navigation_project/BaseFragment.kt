9ca57813b96de246a0768db3c729edbc7863582d
Bruno Barbosa <bruno.barbosa@usemobile.xyz>
2021-05-28 11:26:33 -0300

    feat: create a base fragment to reduce significantly code volume

---
+++ app/src/main/java/bruno_brabosa/convidados/navigation_project/BaseFragment.kt
@@ -0,0 +1,34 @@
+package bruno_brabosa.convidados.navigation_project
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<T : ViewBinding> : Fragment() {
+
+    protected lateinit var binding: T
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = getViewBinding()
+        setUpClicksListeners()
+
+        return binding.root
+    }
+
+    abstract fun getViewBinding(): T
+
+    open fun setUpClicksListeners() {
+        setUpClickNext()
+        setUpClickBack()
+    }
+
+    open fun setUpClickNext() {}
+    open fun setUpClickBack() {}
+}
